c8818a97a40fbbd34baec2fb95e97608e597713a
Matt Keys <Matthewjkeys22@gmail.com>
2021-10-09 16:36:56 -0600

    audit function with .each function in place

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var currentDayEl = $('#currentDay')
-    .text(moment().format('MMMM DD YYYY - hh:ss'));
+    .text(moment().format('MMMM DD YYYY - hh:mm:ss'));
 var businessHours = [9,10,11,12,13,14,15,16,17];
 var currentHour = moment().hour();
+var hourContainer = $('#container')
+
+//interval for clock at top of page
+function updateClock() {
+    $('.clock').html(moment().format('MMMM DD YYYY - hh:mm:ss'));
+  }
+
+setInterval(updateClock, 1000);
 
 //this function runs a for loop to dynamically create the rows
 function showHourEls(){
@@ -10,16 +18,16 @@ function showHourEls(){
         var row = $("<div class='row'>");
         var col1 = $("<div class='col d-flex hour'>");
 
-        var getBusinessHours = businessHours[i] + " AM";
+        var getBusinessHours = businessHours[i] + "AM";
 
         if(businessHours[i] >= 12){
-            getBusinessHours = businessHours[i] + " PM";
+            getBusinessHours = businessHours[i] + "PM";
             if(businessHours[i] >= 13){
                 getBusinessHours = businessHours[i] - 12 + " PM";
             }
         };
     //create input areas
-    var inputArea = $("<input type='text' class='user-input form-control col-10' placeholder='Whatcha got going on?' aria-label='Recipients username' aria-describedby='button-addon2'>")
+    var inputArea = $("<input type='text' class='user-input form-control  col-md-10 col-8' placeholder='Whatcha got going on?' aria-label='Recipients username' aria-describedby='button-addon2'>")
     
     //create buttons
     var hourButton = $("<button class='btn btn-outline-dark saveBtn col' type='button' id='button-addon2'><i class='fas fa-save'></i></button>")
@@ -39,4 +47,17 @@ function showHourEls(){
     } 
 };
 
+//function to audit each hour and color code it
+function auditHours() {
+    hourContainer.children().each(function() {
+        var timeEl = $(this).children('.hour');
+        var timetext = timeEl.text().trim();
+        var time = moment(timetext, "LT");
+        console.log('time moment object', time);
+    })
+} 
+        
+
+
 showHourEls();
+auditHours();
